8a41b1ad42456ab32b5a4a563f07d5b9da674dfb
daviddyess <david.dyess@gmail.com>
2019-12-29 00:05:28 -0700

    Added previous/next book navigation to book chapter list

--- src/pages/Book.js
+++ src/pages/Book.js
@@ -5,22 +5,35 @@ import React, { Component, Fragment } from 'react';
 import { Helmet } from 'react-helmet';
 import { Link } from 'react-router-dom';
 import { Button, Container } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { actions as bookActions } from '../redux/reducers/book';
 import { getBook } from '../redux/selectors/book';
+import { getNavigation } from '../redux/selectors/navigator';
 
 export class Book extends Component {
   static propTypes = {
     actions: PropTypes.object.isRequired,
-    collection: PropTypes.array,
-    match: PropTypes.object
+    collection: PropTypes.object,
+    match: PropTypes.object,
+    navigation: PropTypes.object
   };
+  constructor(props){
+    super(props);
+
+    this.updateBook = this.updateBook.bind(this);
+  }
   componentDidMount() {
     const { actions, match: { params } } = this.props;
     
     actions.requestBook(params.book);
   }
+  updateBook(book){
+    const { actions } = this.props;
+
+    actions.requestBook(book);
+  }
   render() {
-    const { collection } = this.props;
+    const { collection, navigation } = this.props;
     let chapters = [];
     for (var i = 1; i <= collection.chapters; i++) {
       chapters[i] = i;
@@ -33,13 +46,21 @@ export class Book extends Component {
 
           <h1>Bible</h1>
           <h2>{collection.name}</h2>
-          
+          {navigation.previous && navigation.previous.book !== null &&
+          <Button variant="light" size="lg" onClick={(e) => this.updateBook(navigation.previous.book.slug)} as={Link} to={'/bible/'+navigation.previous.book.slug} className="mr-2 mt-2">     
+            <FontAwesomeIcon icon="chevron-left" /> {navigation.previous.book.name}
+          </Button>
+          }
           {chapters.map((chapter,index) => {
             return(
-              <Button variant="light" size="lg" as={Link} to={'/bible/'+collection.slug+'/'+chapter} className="mr-2 mt-2">{chapter}</Button>
+              <Button variant="light" size="lg" as={Link} to={'/bible/'+collection.slug+'/'+chapter} className="mr-2 mt-2" key={`chapter_${chapter}`}>{chapter}</Button>
             )
           })}
-          
+          {navigation.next && navigation.next.book !== null &&
+          <Button variant="light" size="lg" onClick={(e) => this.updateBook(navigation.next.book.slug)} as={Link} to={'/bible/'+navigation.next.book.slug} className="mr-2 mt-2">     
+            {navigation.next.book.name} <FontAwesomeIcon icon="chevron-right" />
+          </Button>
+          }
         </Container>
       </Fragment>
     );
@@ -47,7 +68,8 @@ export class Book extends Component {
 }
 
 const mapStateToProps = state => ({
-  collection: getBook(state)
+  collection: getBook(state),
+  navigation: getNavigation(state)
 });
 
 const mapDispatchToProps = dispatch => ({
