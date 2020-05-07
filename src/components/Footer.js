06ea320109dacfd1150abad0322599d6dbb2b5e7
daviddyess <david.dyess@gmail.com>
2020-05-07 10:07:24 -0600

    Improved Footer styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
-import { Container } from 'react-bootstrap';
+import { Col, Container } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default class Footer extends Component {
   render() {
     return (
-      <Container className="pt-3 pb-2 text-center">
-        &copy; 2018-2020 ProfoundGrace.org | Powered by{' '}
-        <FontAwesomeIcon color="red" icon={['far', 'heart']} /> &amp;{' '}
-        <a href="https://github.com/profoundgrace">Open Source</a>
+      <Container fluid>
+        <Col className="border-top border-light mt-3 pt-2 pb-2 text-right">
+          <p>
+            &copy; 2018-2020 ProfoundGrace.org | Powered by{' '}
+            <FontAwesomeIcon color="red" icon={['far', 'heart']} /> &amp;{' '}
+            <a href="https://github.com/profoundgrace">Open Source</a>
+          </p>
+        </Col>
       </Container>
     );
   }
